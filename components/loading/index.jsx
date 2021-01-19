32a78ef53dd82a6aa3c59f2ba8ac76bcd7fd1dca
Dafrok <o.o@mug.dog>
2021-01-20 01:29:45 +0800

    loading warning

--- components/loading/index.jsx
+++ components/loading/index.jsx
@@ -7,7 +7,7 @@ import * as React from 'react';
 import './loading.styl';
 
 export default function loading({size}) {
-    return <span class="lds-ring" style={{
+    return <span className="lds-ring" style={{
         width: size,
         height: size
     }}><span /><span /><span /><span /></span>;
