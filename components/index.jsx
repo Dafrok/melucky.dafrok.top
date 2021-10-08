c4249e99fad3649aefb65412528550b083477378
Dafrok <o.o@mug.dog>
2021-10-09 00:56:22 +0800

    fix: reconnect

--- components/index.jsx
+++ components/index.jsx
@@ -68,6 +68,7 @@ export default function app() {
             stopSignUp();
         };
         connection.onReconnect = function () {
+            stopSignUp();
             setRoomId(roomId);
         }
         connection.onDanmu = function (res) {
