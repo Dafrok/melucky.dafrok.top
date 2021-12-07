af5b31321fba54ae66b600cca0e3cb7ed99b261a
Dafrok <o.o@mug.dog>
2021-12-07 21:57:28 +0800

    manual add participants

--- components/participants/index.jsx
+++ components/participants/index.jsx
@@ -5,9 +5,11 @@
 
 import * as React from 'react';
 import './style.styl';
+import avatar from '../../resources/card.png';
  
 export default function participants({participants, addParticipant, deleteParticipant, addMember}) {
     const pList = Object.values(participants);
+    const [uname, setUname] = React.useState('');
 
     function selectParticipant(participant) {
         return e => {
@@ -23,8 +25,30 @@ export default function participants({participants, addParticipant, deletePartic
         }
     }
 
+    function manualAddParticipant() {
+        const member = {
+            uid: Date.now(),
+            uname,
+            avatar
+        };
+        addParticipant(member);
+        setUname('');
+    }
+
+    function changeUname(e) {
+        setUname(e.target.value);
+    }
+
     return <div className="participants">
         <ul>
+            <li>
+                <input
+                    style={{width: 135}}
+                    placeholder="请输入昵称"
+                    onChange={changeUname}
+                />
+                <button onClick={manualAddParticipant}>+</button>
+            </li>
             {
                 pList.map(participant => <li key={participant.uid} onClick={selectParticipant(participant)}>
                     <img src={participant.avatar} /><span>{participant.uname}</span>
